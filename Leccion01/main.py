68a5d11a4ed12890bd1349b7288d351da03df1ff
Samuel Perez <sernasamuelperez@gmail.com>
2023-12-20 14:49:58 -0500

    Seccion 3 completada

--- Leccion01/main.py
+++ Leccion01/main.py
@@ -1,2 +1,35 @@
 #Enviar un salido a la consola utilizando python
-print("Este es mi Saludos desde python")
+print("Este es mi Saludo desde python")
+print("=" * 50)
+#Video 8 (No han creado otra carpeta)
+miVariable = 2
+print(miVariable)
+print(miVariable)
+print(miVariable)
+
+miVariable = 10
+print(miVariable)
+print("=" * 50)
+#Video 9 
+x = 10 
+y = 2
+z = x + y
+print("Valor de x = " , x)
+print("Valor de y = " , y)
+print("Valor de z = " , z)
+print("=" * 50)
+
+#Video 10 (Direccion de memoria y variables en python)
+print("La direccion en memoria de x es = " , id(x))
+print("La direccion en memoria de y es = " , id(y))
+print("La direccion en memoria de z es = " , id(z))
+print("=" * 50)
+#Video 11 (Ejercicio Propuesto Uso de Variables)
+#Declaracion de variables
+nombre = "Juan Perez"
+numero = 55621817
+gmail = "jperez@gmail.com"
+
+print("El nombre es =" , nombre)
+print("El numero es = " , numero)
+print("El correo es = " , gmail)
