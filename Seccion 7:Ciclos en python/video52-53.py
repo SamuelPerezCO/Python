c89dde5467f9c0026c08ffe93c5ec26373f4153b
Samuel Perez <sernasamuelperez@gmail.com>
2024-01-16 13:54:59 -0500

    Terminacion seccion 7

---
+++ Seccion 7:Ciclos en python/video52-53.py
@@ -0,0 +1,7 @@
+numero = 0
+
+while numero < 6:
+    print(f'el numero es {numero}')
+    numero += 1
+else:
+    print('Fin del ciclo')
---
+++ Seccion 7:Ciclos en python/video54-55.py
@@ -0,0 +1,7 @@
+numero = 5
+
+while numero >= 0:
+    print(f'El numero es {numero}')
+    numero -= 1
+else:
+    print('Fin del ciclo')
