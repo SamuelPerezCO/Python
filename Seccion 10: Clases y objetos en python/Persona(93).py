2aced141a894ba20d26ccfd2d8d85ee53285933d
Samuel Perez <sernasamuelperez@gmail.com>
2024-01-29 05:46:37 -0500

    Hasta el video 95

---
+++ Seccion 10: Clases y objetos en python/Persona(93).py
@@ -0,0 +1,13 @@
+class Persona:
+    def __init__(self , nombre , apellido , edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+persona1 = Persona('Juan' , 'Perez' , 28)
+print(f'Objeto Persona 1 Nombre: {persona1.nombre} Apellido: {persona1.apellido} Edad: {persona1.edad}')
+
+persona2 = Persona('Karla' , 'Gomez' , 30)
+print(f'Objeto Persona 2 Nombre: {persona2.nombre} Apellido:{persona2.apellido} Edad: {persona2.edad}')
+
+#Referencias de Memoria de objetos y Ejecucion paso a paso
---
+++ Seccion 10: Clases y objetos en python/Persona(94).py
@@ -0,0 +1,17 @@
+class Persona:
+    def __init__(self , nombre , apellido , edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+persona1 = Persona('Juan' , 'Perez' , 28)
+print(f'Objeto Persona 1 Nombre: {persona1.nombre} Apellido: {persona1.apellido} Edad: {persona1.edad}')
+
+persona1.nombre = 'Juan Carlos'
+persona1.apellido = 'Juarez'
+persona1.edad = 25
+print(f'Objeto Persona 1 Nombre: {persona1.nombre} Apellido: {persona1.apellido} Edad: {persona1.edad}')
+
+persona2 = Persona('Karla' , 'Gomez' , 30)
+print(f'Objeto Persona 2 Nombre: {persona2.nombre} Apellido:{persona2.apellido} Edad: {persona2.edad}')
+
