8d2f963552e46f81a614aa13638a6e6ab27e262a
Samuel Perez <sernasamuelperez@gmail.com>
2024-02-01 15:08:25 -0500

    Video 128

--- Seccion 15:Contexto Estatico (De Clase) en Python/MiClase.py
+++ Seccion 15:Contexto Estatico (De Clase) en Python/MiClase.py
@@ -5,15 +5,12 @@ class MiClase:
     def __init__(self, variable_instancia):
         self.variable_instancia = variable_instancia
 
-print(MiClase.variable_clase)
-miClase = MiClase('Valor variable instancia')
-print(miClase.variable_instancia)
-print(miClase.variable_clase)
+    @staticmethod
+    def metodo_estatico():
+        print(MiClase.variable_clase)
 
-MiClase.variable_clase2 = 'Valor variable clase 2'
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
 
-miClase2 = MiClase('Otro Valor de variable instancia')
-print(miClase2.variable_instancia)
-print(miClase.variable_clase)
-print(MiClase.variable_clase2)
-print(miClase.variable_clase2)
+MiClase.metodo_clase()
